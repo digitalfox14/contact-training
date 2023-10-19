35892e03ff4f3b8c0c3f040b5083f908390a1c56
Digitalfox <webartisan.digitalfox@gmail.com>
2023-10-19 16:25:10 +0530

    Test commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user = Auth::user();
         $data = Contact::get();
 
      return view('home', ['data' => $data]);
